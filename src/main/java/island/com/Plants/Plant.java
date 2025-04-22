0ec13a49ae175bfb572198e9d834003866958569
Litvinenko_Igor <java.study653@gmail.com>
2025-04-22 11:24:40 +0300

    add abstract Plant class

---
+++ src/main/java/island/com/Plants/Plant.java
@@ -0,0 +1,20 @@
+package island.com.Plants;
+
+public abstract class Plant {
+    public abstract String getSymbol();
+    public abstract String getSpeciesName();
+    public abstract double getWeight();
+    protected int x;
+    protected int y;
+    protected Plant(int startX, int startY){
+        this.x = startX;
+        this.y = startY;
+    }
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+}
