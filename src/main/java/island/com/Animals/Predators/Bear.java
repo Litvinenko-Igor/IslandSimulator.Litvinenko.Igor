c05f7a50a8735755b10bdc66d772cec8aa3678f5
Litvinenko_Igor <java.study653@gmail.com>
2025-04-22 11:22:29 +0300

    add Predators animals

---
+++ src/main/java/island/com/Animals/Predators/Bear.java
@@ -0,0 +1,77 @@
+package island.com.Animals.Predators;
+
+import island.com.Map.Location;
+import island.com.Plants.Plant;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Bear extends Predators{
+    private static double BASE_WEIGHT = 500.0; // Базова вага виду
+    private static int MAX_COUNT_PER_CELL = 5; // Макс. на клітинці
+    private static final int MAX_SPEED = 2; // Макс. швидкість (клітинок за хід)
+    private static double FOOD_CAPACITY = 80.0; // Потрібно їжі для насичення
+
+    public Bear(int startX, int startY){
+        super(startX, startY);
+        setInitialSaturation(getFoodCapacity() * (0.5 + ThreadLocalRandom.current().nextDouble() * 0.5));
+    }
+
+    @Override
+    public void eat(Location currentLocation) {
+        super.eat(currentLocation);
+        if(this.isAlive() && getCurrentSaturation() < getFoodCapacity()){
+            eatPlants(currentLocation);
+        }
+        // Ну вдруг у нас мишка не зможе бігати за швидкими кроликами то хай спробує травку може сподобається =)
+    }
+    public void eatPlants(Location currentLocation){
+        List<Plant> plantInLocation = currentLocation.getPlants();
+        if(!plantInLocation.isEmpty()){
+            Plant plantToEat  = plantInLocation.get(ThreadLocalRandom.current().nextInt(plantInLocation.size()));
+            double foodWeight = plantToEat.getWeight();
+            if(foodWeight <= 0){
+                return;
+            }
+            double neededSaturation = FOOD_CAPACITY - getCurrentSaturation();
+            double gainedSaturation = Math.min(neededSaturation, foodWeight);
+            this.addSaturation(gainedSaturation);
+            currentLocation.removePlants(plantToEat);
+            System.out.println(this.getSymbol() + " ate " + plantToEat.getSymbol() + " (plant) at (" + this.x + "," + this.y + ")");
+        }
+    }
+
+
+
+    @Override
+    public String getSymbol() {
+        return "🐻";
+    }
+
+    public double getFoodCapacity() {
+        return FOOD_CAPACITY;
+    }
+
+    public int getMaxCountPerCell() {
+        return MAX_COUNT_PER_CELL;
+    }
+
+    public double getWeight() {
+        return BASE_WEIGHT;
+    }
+    @Override
+    public String getSpeciesName() {
+        return "Bear";
+    }
+
+    @Override
+    protected int getMaxSpeed() {
+        return MAX_SPEED;
+    }
+
+    @Override
+    protected double getReproductionChance() {
+        return 0.1;
+    }
+
+}
---
+++ src/main/java/island/com/Animals/Predators/Eagle.java
@@ -0,0 +1,58 @@
+package island.com.Animals.Predators;
+
+import island.com.Map.Location;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Eagle extends Predators{
+    private static double BASE_WEIGHT = 6.0; // Базова вага виду
+    private static int MAX_COUNT_PER_CELL = 20; // Макс. на клітинці
+    private static final int MAX_SPEED = 3; // Макс. швидкість (клітинок за хід)
+    private static double FOOD_CAPACITY = 1.0; // Потрібно їжі для насичення
+
+    public Eagle(int startX, int startY){
+        super(startX,startY);
+        setInitialSaturation(getFoodCapacity() * (0.5 + ThreadLocalRandom.current().nextDouble() * 0.5));
+    }
+
+
+    public void eat(Location currentLocation){
+        super.eat(currentLocation);
+    }
+
+    @Override
+    public String getSymbol() {
+        return "🦅";
+    }
+
+    @Override
+    public double getFoodCapacity() {
+        return FOOD_CAPACITY;
+    }
+
+    @Override
+    public int getMaxCountPerCell() {
+        return MAX_COUNT_PER_CELL;
+    }
+
+    @Override
+    public double getWeight() {
+        return BASE_WEIGHT;
+    }
+
+    @Override
+    public String getSpeciesName() {
+        return "Eagle";
+    }
+
+    @Override
+    protected int getMaxSpeed() {
+        return MAX_SPEED;
+    }
+
+    @Override
+    protected double getReproductionChance() {
+        return 0.2;
+    }
+
+}
